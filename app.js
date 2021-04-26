5a1143cf983b690ffb126049a8d02bdaf84be4c9
Masha <masha.kozhuh04@gmail.com>
2021-04-26 22:16:30 +0200

    add block search

--- app.js
+++ app.js
@@ -19,6 +19,25 @@ const navSider = ()=>{
     });
     
 }
+//search blog
+const clearIcon = document.querySelector(".clear-icon");
+const searchBar = document.querySelector(".search");
+
+searchBar.addEventListener("keyup", () => {
+  if(searchBar.value && clearIcon.style.visibility != "visible"){
+    clearIcon.style.visibility = "visible";
+  } else if(!searchBar.value) {
+    clearIcon.style.visibility = "hidden";
+  }
+});
+
+clearIcon.addEventListener("click", () => {
+  searchBar.value = "";
+  clearIcon.style.visibility = "hidden";
+})
+
+
+//animation
 navSider();
 const logo = document.querySelector('.logo__link');
 const burger = document.querySelector('.burger__menu');
